1a98619841742f43c159dff0eac7b206cdc25b2d
galicians <galicians@gmail.com>
2014-10-04 16:09:14 +0100

    first scenario tested

--- features/step_definitions/joining_game_steps.rb
+++ features/step_definitions/joining_game_steps.rb
@@ -3,6 +3,13 @@ Given(/^I am in the home page$/) do
 	visit '/'
 end
 
-When(/^I register$/) do
-	
+When(/^I register my "(.*?)"$/) do |name|
+	click_link('Register')
+	visit('/registration')
+	fill_in('name', :with => 'name')
+	click_button('Join')	
+end
+
+Then(/^I should be able to see the playing area$/) do
+	visit('/playing_area')
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,34 @@
 require 'sinatra/base'
 
 class Game < Sinatra::Base
-  get '/' do
-    'Hello Game!'
+  
+  set :views, Proc.new { File.join(root, "..","views") }
+  enable :sessions
+  set :public_folder, 'public'
+
+
+  get '/test' do
+
+  	erb :test
   end
+
+  get '/' do
+    
+    erb :index
+  end
+
+  get '/registration' do
+
+  	erb :sign_in
+  end
+
+  post '/playing_area' do
+  	@name = params[:name]
+  	erb :playing_area
+	end
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
